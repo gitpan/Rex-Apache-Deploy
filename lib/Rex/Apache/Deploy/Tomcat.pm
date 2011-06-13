2ce3ab42a48ca855bc653ed557e0e89e01d84bf0
Jan Gehring <cpan@imail.com>
2011-06-13 16:11:30 +0000

    Import of JFRIED/Rex-Apache-Deploy-0.5.0 from CPAN.
    gitpan-cpan-distribution: Rex-Apache-Deploy
    gitpan-cpan-version:      0.5.0
    gitpan-cpan-path:         JFRIED/Rex-Apache-Deploy-0.5.0.tar.gz
    gitpan-cpan-author:       JFRIED
    gitpan-cpan-maturity:     released

---
+++ lib/Rex/Apache/Deploy/Tomcat.pm
@@ -0,0 +1,167 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::Apache::Deploy::Tomcat;
+
+=begin
+
+=head2 SYNOPSIS
+
+This is a (R)?ex module to ease the deployments of PHP, Perl or other languages.
+
+=cut
+
+use strict;
+use warnings;
+
+use Rex::Commands::Run;
+use Rex::Commands::Fs;
+use Rex::Commands::Upload;
+use Rex::Commands;
+use File::Basename qw(dirname basename);
+use LWP::UserAgent;
+
+#require Exporter;
+#use base qw(Exporter);
+
+use vars qw(@EXPORT $context_path);
+@EXPORT = qw(deploy context_path);
+
+############ deploy functions ################
+
+sub deploy {
+
+   my ($file, @option) = @_;
+
+   my $options = { @option };
+
+   no strict;
+   no warnings;
+   my $rnd_file = get_random(8, a..z, 0..9);
+   use strict;
+   use warnings;
+
+   if(! exists $options->{"context_path"}) {
+      $options->{"context_path"} = $context_path;
+   }
+
+   if(exists $options->{"manager_url"}) {
+      my $mgr_url = $options->{"manager_url"};
+      $mgr_url =~ s{^/}{};
+      $options->{"manager_url"} = $mgr_url;
+   } 
+   else {
+      $options->{"manager_url"} = "manager";
+   }
+
+   upload ($file, "/tmp/$rnd_file.war");
+
+   $options->{"file"} = "/tmp/$rnd_file.war";
+
+   # zuerst muss undeployed werden
+   _undeploy($options);
+
+   # und dann wieder deployen
+   _deploy($options);
+
+
+   unlink "/tmp/$rnd_file.war";
+}
+
+############ helper function ##############
+
+sub _deploy {
+
+   my $p = shift;
+
+   my $current_connection = Rex::get_current_connection();
+
+	my $ua = LWP::UserAgent->new();
+   my $resp = $ua->get(_get_url($current_connection->{"server"} . ":" . $p->{'port'},
+                                 "deploy?path=" . $p->{"context_path"} . "&war=file:" . $p->{"file"}, 
+                                 $p->{"username"}, 
+                                 $p->{"password"},
+                                 $p->{"manager_url"}));
+   if($resp->is_success) {
+      Rex::Logger::info($resp->decoded_content);
+   } else {
+      Rex::Logger::info("FAILURE: " . $current_connection->{"server"} . ": " . $resp->status_line);
+   }
+
+}
+
+sub _undeploy {
+
+   my $p = shift;
+	
+	_do_action("undeploy", 
+               $p->{"context_path"},
+               $p->{"port"},
+               $p->{"username"},
+               $p->{"password"},
+               $p->{"manager_url"});
+
+}
+
+sub _get_url {
+	my $server = shift;
+	my $command = shift;
+   my $user = shift;
+   my $pw = shift;
+   my $mgr_path = shift;
+
+   $mgr_path ||= "manager";
+	
+	return "http://$user:$pw\@" . "$server/$mgr_path/$command";
+}
+
+
+sub _do_action {
+
+   my $action = shift;
+	my $path   = shift;
+   my $port   = shift;
+   my $user   = shift;
+   my $pw     = shift;
+   my $mgr_path = shift;
+
+   $mgr_path ||= "manager";
+
+	my $ua = LWP::UserAgent->new();
+   my $current_connection = Rex::get_current_connection();
+
+   my $_url = _get_url($current_connection->{"server"} . ":".$port, "$action?path=$path", $user, $pw, $mgr_path);
+   Rex::Logger::debug("Connecting to: $_url");
+
+   my $resp = $ua->get($_url);
+   if($resp->is_success) {
+      Rex::Logger::info($resp->decoded_content);
+   } else {
+      Rex::Logger::info("FAILURE: " . $current_connection->{"server"} . ":$port:  " . $resp->status_line);
+   }
+
+}
+
+
+############ configuration functions #############
+
+sub context_path {
+   $context_path = shift;
+}
+
+####### import function #######
+
+sub import {
+
+   no strict 'refs';
+   for my $func (@EXPORT) {
+      Rex::Logger::debug("Registering main::$func");
+      *{"$_[1]::$func"} = \&$func;
+   }
+
+}
+
+1;
---
+++ lib/Rex/Apache/Inject/Template.pm
@@ -0,0 +1,223 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::Apache::Inject::Template;
+
+use strict;
+use warnings;
+
+use Rex::Commands::Run;
+use Rex::Commands::Fs;
+use Rex::Commands::Upload;
+use Rex::Commands;
+use File::Basename qw(dirname basename);
+use Cwd qw(getcwd);
+
+#require Exporter;
+#use base qw(Exporter);
+
+use vars qw(@EXPORT $real_name_from_template $template_file $template_pattern);
+@EXPORT = qw(inject
+               generate_real_name
+               template_file template_search_for);
+
+my $work_dir = getcwd;
+
+############ deploy functions ################
+
+sub inject {
+   my ($to, @options) = @_;
+
+   my $option = { @options };
+
+   my $cmd1 = sprintf (_get_extract_command($to), "../$to");
+   my $cmd2 = sprintf (_get_pack_command($to), "../$to", ".");
+
+   my $template_params = _get_template_params($template_file);
+
+   mkdir("tmp");
+   chdir("tmp");
+   run $cmd1;
+
+   if(exists $option->{"extract"}) {
+      for my $file_pattern (@{$option->{"extract"}}) {
+
+         for my $found_file (`find ../ -name '$file_pattern'`) {
+            chomp $found_file;
+
+            mkdir "tmp-b";
+            chdir "tmp-b";
+
+            my $extract_cmd  = sprintf( _get_extract_command($found_file), "../$found_file" );
+            my $compress_cmd = sprintf( _get_pack_command($found_file), "../$found_file", "." );
+
+            run $extract_cmd;
+
+            _find_and_parse_templates();
+
+            if(exists $option->{"pre_pack_hook"}) {
+               &{ $option->{"pre_pack_hook"} }($found_file);
+            }
+
+            run $compress_cmd;
+
+            if(exists $option->{"post_pack_hook"}) {
+               &{ $option->{"post_pack_hook"} }($found_file);
+            }
+
+            chdir "..";
+            rmdir "tmp-b";
+         }
+
+      }
+   }
+
+   _find_and_parse_templates();
+
+   if(exists $option->{"pre_pack_hook"}) {
+      &{ $option->{"pre_pack_hook"} };
+   }
+
+   run $cmd2;
+
+   if(exists $option->{"post_pack_hook"}) {
+      &{ $option->{"post_pack_hook"} };
+   }
+
+   chdir("..");
+   system("rm -rf tmp");
+}
+
+sub _find_and_parse_templates {
+
+   my $template_params = _get_template_params($template_file);
+
+   for my $file (`find . -name '$template_pattern'`) {
+      chomp $file;
+      my $content = eval { local(@ARGV, $/) = ($file); $_=<>; $_; };
+      for my $key (keys %$template_params) {
+         my $val = $template_params->{$key};
+         if($content =~ m/\@$key\@/gm) {
+            Rex::Logger::info("Replacing \@$key\@ with $val ($file)");
+            $content =~ s/\@$key\@/$val/g;
+         }
+      }
+
+      my $new_file_name = $real_name_from_template?&$real_name_from_template($file):$file;
+      open(my $fh, ">", $new_file_name) or die($!);
+      print $fh $content;
+      close($fh);
+   }
+
+
+}
+
+
+############ configuration functions #############
+
+sub generate_real_name {
+   $real_name_from_template = shift;
+}
+
+sub template_file {
+   $template_file = shift;
+}
+
+sub template_search_for {
+   $template_pattern = shift;
+}
+
+############ helper functions #############
+
+sub _get_extract_command {
+   my ($file) = @_;
+
+   if($file =~ m/\.tar\.gz$/) {
+      return "tar xzf %s";
+   } elsif($file =~ m/\.zip$/) {
+      return "unzip %s";
+   } elsif($file =~ m/\.tar\.bz2$/) {
+      return "tar xjf %s";
+   } elsif($file =~ m/\.war$/) {
+      return "unzip %s";
+   } elsif($file =~ m/\.jar$/) {
+      return "unzip %s";
+   }
+
+   die("Unknown Archive Format.");
+}
+
+sub _get_pack_command {
+   my ($file) = @_;
+
+   if($file =~ m/\.tar\.gz$/) {
+      return "tar czf %s %s";
+   } elsif($file =~ m/\.zip$/) {
+      return "zip -r %s %s";
+   } elsif($file =~ m/\.tar\.bz2$/) {
+      return "tar cjf %s %s";
+   } elsif($file =~ m/\.war$/) {
+      return "zip -r %s %s";
+   } elsif($file =~ m/\.jar$/) {
+      return "zip -r %s %s";
+   }
+
+   die("Unknown Archive Format.");
+}
+
+# read the template file and return a hashref.
+sub _get_template_params {
+   my ($template_file) = @_;
+   my @lines = eval { local(@ARGV) = ($work_dir . "/" . $template_file); <>; };
+   my $r = {};
+   for my $line (@lines) {
+      next if ($line =~ m/^#/);
+      next if ($line =~ m/^\s*?$/);
+
+      my ($key, $val) = ($line =~ m/^(.*?) ?= ?(.*)$/);
+      $val =~ s/^["']//;
+      $val =~ s/["']$//;
+
+      $r->{$key} = $val;
+   }
+
+   $r;
+}
+
+sub _get_ext {
+   my ($file) = @_;
+
+   if($file =~ m/\.tar\.gz$/) {
+      return ".tar.gz";
+   } elsif($file =~ m/\.zip$/) {
+      return ".zip";
+   } elsif($file =~ m/\.tar\.bz2$/) {
+      return ".tar.bz2";
+   } elsif($file =~ m/\.war$/) {
+      return ".war";
+   } elsif($file =~ m/\.jar$/) {
+      return ".jar";
+   }
+
+   die("Unknown Archive Format.");
+
+}
+
+####### import function #######
+
+sub import {
+
+   no strict 'refs';
+   for my $func (@EXPORT) {
+      Rex::Logger::debug("Registering main::$func");
+      *{"$_[1]::$func"} = \&$func;
+   }
+
+}
+
+
+
+1;
